585a2c4e1ac6468b1092d3a45396b32147f87257
Bence Eros <crystal@cyclonephp.org>
2012-06-24 21:30:15 +0200

    adding docs to AttributePosition class

--- AttributePosition.js
+++ AttributePosition.js
@@ -1,8 +1,32 @@
 (function($) {
-	
+
+    /**
+     * Contains information about the position of a NodeController appearing in a DOM attribute.
+     *
+     * @class
+     */
 	var AttributePosition = $.wiredui.AttributePosition = function(elem, attrName) {
+
+        /**
+         * The DOM element which contains the attribute which the controller belongs to.
+         *
+         * @type DOMElem
+         */
 		this.origParentElem = elem;
+
+        /**
+         * The name of the attribute the controller should affect.
+         *
+         * @type {*}
+         */
 		this.attrName = attrName;
+
+        /**
+         * RunID => DOMElem pairs where the DOM elem is the elem containing the attribute which'
+         * value the controller should affect.
+         *
+         * @type {Object}
+         */
         this.parentElems = {};
 	}
 	
